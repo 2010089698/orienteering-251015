66c7d954c549461fd7a324ba3de6393140f8df62
2010089698 <2010089698@campus.ouj.ac.jp>
2025-10-15 16:07:44 +0900

    Switch tests to Jest globals

--- StartlistManagement/Domain/src/startlist/events/__tests__/StartlistEvents.test.ts
+++ StartlistManagement/Domain/src/startlist/events/__tests__/StartlistEvents.test.ts
@@ -1,5 +1,5 @@
 import assert from 'node:assert/strict';
-import test from 'node:test';
+import { test } from '@jest/globals';
 
 import { ClassAssignment } from '../../../startlist/ClassAssignment';
 import { Duration } from '../../../startlist/Duration';
